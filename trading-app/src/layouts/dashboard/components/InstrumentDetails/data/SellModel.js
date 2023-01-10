2cfca2c611d6d5fee028195f5e72f7a9b866be72
sachingour <gour.sachin.sg@gmail.com>
2023-01-10 11:51:43 +0530

    Buy Sell Button Configure

--- trading-app/src/layouts/dashboard/components/InstrumentDetails/data/SellModel.js
+++ trading-app/src/layouts/dashboard/components/InstrumentDetails/data/SellModel.js
@@ -7,19 +7,37 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
-// import MDButton from '../../components/MDButton';
 import TextField from '@mui/material/TextField';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import MDButton from '../../../../../components/MDButton';
-
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import { Box, Typography } from '@mui/material';
 
 const SellModel = () => {
   const [open, setOpen] = React.useState(false);
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+
+  const [value, setValue] = React.useState('NRML');
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const [market, setMarket] = React.useState('MARKET');
+  const marketHandleChange = (event) => {
+    setMarket(event.target.value);
+  };
+  const [validity, setValidity] = React.useState('DAY');
+  const validityhandleChange = (event) => {
+    setValidity(event.target.value);
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -31,65 +49,94 @@ const SellModel = () => {
 
   return (
     <div>
-      <MDButton variant="contained" color="error" onClick={handleClickOpen} fullWidth>
-        SELL
-      </MDButton>
-      <Dialog
-        fullScreen={fullScreen}
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="responsive-dialog-title"
-      >
-        <DialogTitle id="responsive-dialog-title">
-          {""}
-        </DialogTitle>
         <DialogContent>
-          {/* <DialogContentText sx={{ display: "flex", flexDirection: "column" }}>
-            <TextField
-              id="outlined-basic" label="Broker" variant="standard"
-              sx={{ margin: 1, padding: 1, width: "300px" }} />
+          <DialogContentText sx={{ display: "flex", flexDirection: "column", marginLeft: 2, marginTop: 1 }}>
+            <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "center", margin: 2 }}><Box sx={{ backgroundColor: "#ccccb3", fontWeight: 600 }}>Symbol</Box> &nbsp; &nbsp; &nbsp; <Box sx={{ backgroundColor: "#ccccb3", fontWeight: 600 }}>LTP</Box></Box>
+            <FormControl >
 
-            <TextField
-              id="outlined-basic" label="Account ID" variant="standard" 
-              sx={{ margin: 1, padding: 1, width: "300px" }} />
-            
-
-            <TextField
-              id="outlined-basic" label="Acccount Name" variant="standard"
-              sx={{ margin: 1, padding: 1, width: "300px" }} />
-
-            
-            <TextField
-              id="outlined-basic" label="API Key" variant="standard" 
-              sx={{ margin: 1, padding: 1, width: "300px" }} />
-
-            <TextField
-              id="outlined-basic" label="API Secret" variant="standard" 
-              sx={{ margin: 1, padding: 1, width: "300px" }} />
-
-            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-              <InputLabel id="demo-simple-select-standard-label">Status</InputLabel>
-              <Select
-                labelId="demo-simple-select-standard-label"
-                id="demo-simple-select-standard"
-                label="Status"
-                sx={{ margin: 1, padding: 1, width: "300px" }}
+              <FormLabel id="demo-controlled-radio-buttons-group" sx={{ width: "300px" }}></FormLabel>
+              <RadioGroup
+                aria-labelledby="demo-controlled-radio-buttons-group"
+                name="controlled-radio-buttons-group"
+                value={value}
+                onChange={handleChange}
+                sx={{ display: "flex", flexDirection: "row" }}
               >
-                <MenuItem value="active">Active</MenuItem>
-                <MenuItem value="inactive">Inctive</MenuItem>
-              </Select>
+                <FormControlLabel value="MIS" control={<Radio />} label="Intraday MIS" />
+                <FormControlLabel value="NRML" control={<Radio />} label="Overnight NRML" />
+              </RadioGroup>
             </FormControl>
-          </DialogContentText> */}
+
+            <Box sx={{ display: "flex", flexDirection: "row" }}>
+              <FormControl variant="standard" sx={{ m: 1, minWidth: 120, }}>
+                <InputLabel id="demo-simple-select-standard-label">Quantity</InputLabel>
+                <Select
+                  labelId="demo-simple-select-standard-label"
+                  id="demo-simple-select-standard"
+                  label="Quantity"
+                  sx={{ margin: 1, padding: 1, }}
+                >
+                  <MenuItem value="100">100</MenuItem>
+                  <MenuItem value="150">150</MenuItem>
+                </Select>
+              </FormControl>
+              <TextField
+                id="outlined-basic" label="Price" variant="standard"
+                sx={{ margin: 1, padding: 1, width: "300px", marginRight: 1, marginLeft: 1 }} />
+
+              <TextField
+                id="outlined-basic" label="Trigger Price" variant="standard"
+                sx={{ margin: 1, padding: 1, width: "300px" }} />
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "flex-end" }}>
+              <FormControl  >
+                <FormLabel id="demo-controlled-radio-buttons-group" ></FormLabel>
+                <RadioGroup
+                  aria-labelledby="demo-controlled-radio-buttons-group"
+                  name="controlled-radio-buttons-group"
+                  value={market}
+                  onChange={marketHandleChange}
+                  sx={{ display: "flex", flexDirection: "row" }}
+                >
+                  <FormControlLabel value="MARKET" control={<Radio />} label="MARKET" />
+                  <FormControlLabel value="LIMIT" control={<Radio />} label="LIMIT" />
+                </RadioGroup>
+              </FormControl>
+              <FormControl  >
+                <FormLabel id="demo-controlled-radio-buttons-group" ></FormLabel>
+                <RadioGroup
+                  aria-labelledby="demo-controlled-radio-buttons-group"
+                  name="controlled-radio-buttons-group"
+                  value={value}
+                  onChange={handleChange}
+                  sx={{ display: "flex", flexDirection: "row" }}
+                >
+                  <FormControlLabel value="SL" control={<Radio />} label="SL" />
+                  <FormControlLabel value="SLM" control={<Radio />} label="SL-M" />
+                </RadioGroup>
+              </FormControl>
+
+            </Box>
+
+            <Box>
+              <FormControl  >
+                <FormLabel id="demo-controlled-radio-buttons-group" >Validity</FormLabel>
+                <RadioGroup
+                  aria-labelledby="demo-controlled-radio-buttons-group"
+                  name="controlled-radio-buttons-group"
+                  value={validity}
+                  onChange={validityhandleChange}
+                  sx={{ display: "flex", flexDirection: "column" }}
+                >
+                  <FormControlLabel value="DAY" control={<Radio />} label="DAY" />
+                  <FormControlLabel value="IMMEDIATE" control={<Radio />} label="IMMEDIATE" />
+                  <FormControlLabel value="MINUTES" control={<Radio />} label="MINUTES" />
+                </RadioGroup>
+              </FormControl>
+            </Box>
+          </DialogContentText>
         </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-           SELL
-          </Button>
-          <Button onClick={handleClose} autoFocus>
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
+       
     </div>
   );
 }
