cd47de5a4f4f99c56a252d42dc99cc07c8282813
vvv201214 <vvv201214@gmail.com>
2023-01-13 15:55:21 +0530

    build

--- server-side/db/conn.js
+++ server-side/db/conn.js
@@ -3,11 +3,13 @@ const dotenv = require("dotenv");
 
 dotenv.config({path: "./config.env"});
 
-
+// STAGINGDB    
 const DB = process.env.DATABASE;
     const devDB = process.env.DEVDATABASE;
+    const stagingDB = process.env.STAGINGDB;
 //mongoose.connect(devDB, {
-    mongoose.connect(DB, {
+    // mongoose.connect(DB, {
+        mongoose.connect(stagingDB, {
     useNewUrlParser: true,
     
     useUnifiedTopology: true,
