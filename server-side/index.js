3c595b5658818f91657ac40ff7a91da558e89ca8
vvv201214 <101820333+vvv201214@users.noreply.github.com>
2023-01-19 09:36:43 +0530

    Update index.js

--- server-side/index.js
+++ server-side/index.js
@@ -18,7 +18,7 @@ const xssClean = require("xss-clean");
 const hpp = require("hpp")
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 15 minutes
-	max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 5000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: "Too many request"
