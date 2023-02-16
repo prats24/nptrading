cd8d8103706d09562acd29a4f6d577381a9038d3
vvv201214 <vvv201214@gmail.com>
2023-02-16 18:47:09 +0530

    fund schema and route

--- server-side/models/marginAllocation/marginAllocationSchema.js
+++ server-side/models/marginAllocation/marginAllocationSchema.js
@@ -24,6 +24,7 @@ const marginSchema = new mongoose.Schema({
     },
     lastModifiedOn:{
         type: Date,
+        default: Date.now(),
         // required : true
     },
     lastModifiedBy:{
