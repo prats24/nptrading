b6d6a72644274e90603f33ee3c3e1f4fc8c73e18
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2023-02-20 12:23:15 +0530

    collectin for margin calls

---
+++ server-side/models/marginAllocation/MarginCall.js
@@ -0,0 +1,85 @@
+const mongoose = require("mongoose");
+
+const MarginCallSchema = new mongoose.Schema({
+    status:{
+        type: String,
+        required: true
+    },
+    uId:{//random string to be generated
+        type: String,
+        required : true
+    },
+    createdBy:{//req body
+        type: String,
+        required : true
+    },
+    average_price:{//price from req body
+        type: Number,
+        required: true
+    },
+    Quantity:{ //req body
+        type: String,
+        required: true
+    },
+    Product:{//req body
+        type: String,
+        required: true
+    },
+    buyOrSell:{//req body
+        type: String,
+        required: true
+    },
+    order_timestamp:{//
+        type: String,
+        required: true
+    },
+    variety:{//req body
+        type: String,
+        required: true
+    },
+    validity:{//req body
+        type: String,
+        required: true
+    },
+    exchange:{ //req body
+        type: String,
+        required: true
+    },
+    order_type:{//req body
+        type: String,
+        required: true
+    },
+    symbol:{//req body
+        type: String,
+        required: true
+    },
+    userId:{ //req body
+        type: String,
+        required: true        
+    },
+    instrumentToken:{//req.body
+        type: String, 
+        required: true 
+    },
+    tradeBy:{ //same as createdBy from req
+        type: String,
+        required: true        
+    },
+    isRealTrade:{//to be figured
+        type: Boolean,
+        required: true  
+    },
+    amount:{//Quantity * last price
+        type: Number,
+        required: true        
+    },
+    trade_time:{//now
+        type: String,
+        required: true        
+    },
+})
+
+const MarginCall = mongoose.model("margin-call", MarginCallSchema);
+module.exports = MarginCall;
+
+
