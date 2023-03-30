48874a4d6b4efe6b177ef03a18739b62bd3c9b7a
Prateek Pawan <prateekpawan@Prateeks-MacBook-Pro.local>
2023-03-30 15:13:13 +0530

    Schema for contest

---
+++ server-side/models/Contest/contestSchema.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const contestSchema = new mongoose.Schema({
+    contestName:{
+        type: String,
+        required: true
+    },
+    contestStartDate:{
+        type: Date,
+        required: true
+    },
+    contestEndDate:{
+        type:Date,
+        required: true
+    },
+    entryOpeningDate:{
+        type:Date,
+        required: true
+    },
+    entryClosingDate:{
+        type:Date,
+        required: true
+    },
+    StockType:{
+        type:String,
+        required:true,
+        enum: ['Options','Futures','Equity','Derivative','Currency','Crypto']
+    },
+    ContestOn:{
+        type:String,
+        required:true
+    },
+    contestRules:{
+        type:[{orderNo:Number,rule:String}],
+        required:true,
+    },
+    rewards:{
+        type:[{rankStart:Number,rankEnd:Number,rankIcon:String,reward:Number,currency:String}],
+        required:true
+    },
+    entryFee:{
+        amount:Number,
+        currency:{type:String,enum:['INR','CREDOS']}
+    },
+    participants:[{
+        userId:{type:Schema.Types.ObjectId, ref: 'user-personal-detail'},
+        registeredOn:{type:Date},
+        paymentId:{type:Schema.Types.ObjectId, ref: 'contest-payment'},
+    }],
+    instruments:[{
+        displayName:String,
+        instrumentSymbol:String,
+        instrumentToken:Number,
+        exchange:String,
+        addedOn: {type:Date,default:new Date()},
+        addedby: {type:Schema.Types.ObjectId,ref:'user-personal-detail'},
+        lastModifiedOn: {type:Date,default:new Date()},
+        lastModifiedBy: {type:Schema.Types.ObjectId,ref:'user-personal-detail'},
+    }],
+    maxParticipants:{
+        type:Number,
+        required: true
+    },
+    minParticipants:{
+        type:Number,
+        required: true
+    },
+    createdOn:{
+        type: Date,
+        required : true,
+        default: new Date(),
+    },
+    lastModifiedOn:{
+        type: Date,
+        required : true,
+        default: new Date(),
+    },
+    createdBy:{
+        type: Schema.Types.ObjectId,
+        ref: 'user-personal-detail',
+        // required : true
+    },
+    lastModifiedBy:{
+        type: Schema.Types.ObjectId,
+        ref: 'user-personal-detail',
+        // required : true
+    },
+})
+
+const contestData = mongoose.model("contest", contestSchema);
+module.exports = contestData;
