89837c5a83cec24f0402e1c7574b5cb6de63b73d
Prateek Pawan <prateekpawan@Prateeks-MacBook-Pro.local>
2023-04-06 01:32:47 +0530

    Join Contest

--- trading-app/src/layouts/user-contest/joinContest/joinContest.js
+++ trading-app/src/layouts/user-contest/joinContest/joinContest.js
@@ -5,7 +5,8 @@ import Grid from "@mui/material/Grid";
 import MDBox from "../../../components/MDBox";
 // import MDButton from "../../components/MDButton";
 // import Paper from '@mui/material/Paper';
-// import MDTypography from "../../../components/MDTypography";
+import MDTypography from "../../../components/MDTypography";
+import Divider from '@mui/material/Divider';
 // import ContestIcon from "../../../assets/images/contest.png";
 // // import ContestDetailsForm from './CreateContest'
 // // import StockIcon from '../../assets/images/contest.gif'
@@ -19,11 +20,23 @@ import "./Style.css"
 const JoinContest = () => {
 console.log("hii")
 
+let rewards = 
+[
+    {rank:1,reward:'INR 200'},
+    {rank:2,reward:'INR 150'},
+    {rank:3,reward:'INR 100'},
+    {rank:4,reward:'INR 80'},
+    {rank:5,reward:'INR 60'},
+    {rank:6,reward:'INR 30'},
+    {rank:7,reward:'INR 10'},
+    {rank:8,reward:'INR 5'}
+]
+
     return (
         <>
         {/* <MDBox pt={0} pb={1}> */}
             {/* <Grid container spacing={2} mt={-4}> */}
-                <div className='container'>
+                {/* <div className='container'>
                     <div className='sub-container1'>
                         <div className='top-text'>League is about to begin <span style={{backgroundColor: 'green', color: "white", padding: "5px", borderRadius: "5px"}}>4 days 11 hours</span></div>
                         <div className='sub-container-part1'>
@@ -78,9 +91,82 @@ console.log("hii")
                     <div className='sub-container3'>
                         rules
                     </div>
-                </div>
+                </div> */}
             {/* </Grid>
         </MDBox> */}
+        <MDBox mt={6} ml={3} width="100%">
+            <Grid container >
+
+                <Grid items xs={12} md={6} lg={4}>
+                    <MDBox>
+
+                        <MDTypography display="flex" justifyContent="center">
+                            League is about to begin in 4 days 11 hours
+                        </MDTypography>
+
+                        <Grid container>
+                            <Grid item>
+                               Hello 
+                            </Grid>
+                        </Grid>
+
+                    </MDBox>
+                </Grid>
+
+                <Divider orientation="vertical" flexItem />
+
+                <Grid items xs={12} md={6} lg={4}>
+                    <MDBox>
+                        <MDTypography display="flex" justifyContent="center">
+                            Reward Distribution
+                        </MDTypography>
+                            <Grid container display="flex" flexDirection="row">
+                                
+                                <Grid item xs={6} md={3} lg={6} display="flex" justifyContent="center">
+                                     <MDTypography>
+                                             Rank
+                                     </MDTypography>
+                                </Grid>
+
+                                <Grid item xs={6} md={3} lg={6} display="flex" justifyContent="center">
+                                     <MDTypography>
+                                             Reward
+                                     </MDTypography>
+                                </Grid>
+
+                                {rewards.map((e)=>{
+                                 return <>
+                                    <Grid item xs={6} md={3} lg={6} display="flex" justifyContent="center">
+                                    <MDTypography>
+                                            {e.rank}
+                                    </MDTypography>
+                                    </Grid>
+
+                                    <Grid item xs={6} md={3} lg={6} display="flex" justifyContent="center">
+                                    <MDTypography>
+                                            {e.reward}
+                                    </MDTypography>
+                                    </Grid>
+                                </>
+                                })}
+
+
+                            </Grid>
+                    </MDBox>
+                </Grid>
+                
+                <Divider orientation="vertical" flexItem />
+
+                <Grid items xs={12} md={6} lg={3}>
+                    <MDBox>
+                        <MDTypography display="flex" justifyContent="center">
+                            League Rules
+                        </MDTypography>
+                    </MDBox>
+                </Grid> 
+
+            </Grid>
+        </MDBox>
       </>
     )
 }
