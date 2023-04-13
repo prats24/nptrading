1e5ea76d162b51d7d15caae71ec7bacb4dcd49c4
Anshuman Sharma <anshumansharma@Anshumans-MacBook-Air.local>
2023-04-13 23:33:18 +0530

    home page basic UI

--- trading-app/src/layouts/UserHome/index.js
+++ trading-app/src/layouts/UserHome/index.js
@@ -3,11 +3,15 @@ import DashboardLayout from "../../examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "../../examples/Navbars/DashboardNavbar";
 import Footer from "../../examples/Footer";
 import { Grid } from "@mui/material";
+import MDBox from "../../components/MDBox";
+import referralImage from '../../assets/images/referral.png';
+import ReportsLineChart from "../../examples/Charts/BarCharts/ReportsBarChart";
 
 
 // Data
 import Carousel from "./data/Carousel";
 import Indicies from "../traderHome/components/IndicesComponent";
+import { display } from "@mui/system";
 
 function Tables() {
 
@@ -19,7 +23,49 @@ function Tables() {
                 <Indicies/>
       </Grid>
       <Carousel/>
-      <Footer />
+      <Grid style={{display: 'flex'}} mt={4}>
+          <MDBox style={{flex:2}}>
+            <MDBox style={{position:'relative'}}>
+              <img src ={referralImage} width='100%' />
+              <MDBox style={{
+                display:'flex', 
+                justifyContent: 'space-between', 
+                position: 'absolute',
+                width: '100%', 
+                bottom: 10,
+                paddingLeft: 10,
+                paddingRight:10, 
+                backgroundColor: '#344666',
+                color: 'white'
+                }}>
+                <span>Winnings</span>
+                <span>₹150.00</span>
+              </MDBox>
+            </MDBox>
+          </MDBox>
+          <Grid style={{flex:3, height:'150px', marginTop:'24px', marginLeft: '12px'}}>
+              <MDBox mb={3} style={{height: '100%'}}>
+                <ReportsLineChart
+                  color="success"
+                  colorheight="14rem"
+                  title="Gross p&l (in INR)"
+                  // description={
+                  //   <>
+                  //     (<strong>+15%</strong>) increase than previous last 5 days.
+                  //   </>
+                  // }
+                  // date="updated just now"
+                  chart={
+                    {
+                      labels: [],
+                      datasets: { label: "Gross P&L", data: [] },
+                    }
+                  }
+                />
+              </MDBox>
+            </Grid>
+      </Grid>
+      {/* <Footer /> */}
     </DashboardLayout>
     </>
   );
